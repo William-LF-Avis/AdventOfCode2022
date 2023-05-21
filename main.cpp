5ad7eae07a45736f0ed7ee25e54e3ab0b82b3c86
William-LF-Avis <william.lewis.avis@outlook.com>
2023-05-21 20:21:07 +0100

    Fixed incorrect input-dir string

--- main.cpp
+++ main.cpp
@@ -12,18 +12,18 @@ int main() {
 	// remove namespace myFilepath
 	const std::string INPUTDIR = myFilepath::PROJECTDIR + R"(TaskInputs\)";
 
-	Day1::runTasks(INPUTDIR + "day1.txt)");
+	Day1::runTasks(INPUTDIR + "day1.txt");
 	std::cout << '\n';
 
-	Day2::runTasks(INPUTDIR + "day2.txt)");
+	Day2::runTasks(INPUTDIR + "day2.txt");
 
-	Day3::Task1(INPUTDIR + "day3.txt)");
-	Day3::Task2(INPUTDIR + "day3.txt)");
+	Day3::Task1(INPUTDIR + "day3.txt");
+	Day3::Task2(INPUTDIR + "day3.txt");
 
-	Day4::Task1(INPUTDIR + "day4.txt)");
-	Day4::Task2(INPUTDIR + "day4.txt)");
+	Day4::Task1(INPUTDIR + "day4.txt");
+	Day4::Task2(INPUTDIR + "day4.txt");
 
-	Day5::runTasks(INPUTDIR + "day5_start.txt)", INPUTDIR + "day5_procedure.txt)");
+	Day5::runTasks(INPUTDIR + "day5_start.txt", INPUTDIR + "day5_procedure.txt");
 
 	return 0;
 }
